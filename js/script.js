3d8e9ebba4e2e3f353e2698a6b321dc6a295141a
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-03-18 16:51:11 +0300

    add

--- js/script.js
+++ js/script.js
@@ -70,10 +70,31 @@ myfunction()*/
 
 function myfunction(){
    var n1=document.getElementById('n1').value;
+   var op=document.getElementById('op').value;
    var n2=document.getElementById('n2').value;
    var result=document.getElementById('result');
+   if(op=='+'){
   var sum=parseInt(n1)+ parseInt(n2);
 result.innerHTML=sum;
-console.log(result)
+console.log(result)}
 
+else if(op=='-'){
+    var sum=parseInt(n1)- parseInt(n2);
+result.innerHTML=sum;
+console.log(result)}
+
+else if(op=='*'){
+    var sum=parseInt(n1)* parseInt(n2);
+result.innerHTML=sum;
+console.log(result)}
+
+else if(op=='/'){
+    var sum=parseInt(n1)/parseInt(n2);
+result.innerHTML=sum;
+console.log(result)}
 }
+
+
+
+
+
