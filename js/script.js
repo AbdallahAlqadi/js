016384d927a87fc18ce36b7952c84f1d2578af2f
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-03-18 09:18:52 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/*alert("Hello world");
+console.log("Abdallah");*/
+
+/*var in java script
+var name="jhon";
+console.log(name);
+ name="Ali";
+console.log(name);*/
+
+/*var in java script
+let age=12;
+console.log(age);
+ age=65;
+console.log(age);*/
+
+/*list in java script
+var person=["Jhon",25,"New York"];
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+person[2]="Amman";
+console.log(person);
+person=100;
+console.log(person);*/
+
+
+/*obj in java script
+var person2={name:"Abdallah",age:15};
+console.log(person2)
+console.log(person2.age)*/
+
+
+var age=100;
+if(age==='100'){
+
+    console.log("is equal")
+}
+else{
+    console.log("is not equal")
+}
